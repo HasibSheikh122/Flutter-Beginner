6e42c23649f353bd8eb199806077f85234cab720
HasibSheikh122 <hasibsk1606@gmail.com>
2025-11-07 00:03:43 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+// MaterialApp | Scaffold | Text | Center | Image | RichText
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Hello world',
+    home: Scaffold(
+      body: Center(
+        child: Text('Hello world'),
+      ),
+    ),
+  ));
+
+}
+
