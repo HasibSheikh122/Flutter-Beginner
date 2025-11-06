fbdc21220e017efa80d522982b721cf22f606101
HasibSheikh122 <hasibsk1606@gmail.com>
2025-11-07 00:43:16 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,58 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp(
-    title: 'Hello world',
-    home: Scaffold(
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+       title: 'Hello world',
+       home: Home()
+   );
+  }
+
+}
+
+
+class Home extends StatelessWidget{
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
-        child: Text('Hello world'),
+        // Text
+        /*
+        child: Text('Hello Flutter, We are starting a new application from today. I am flutter app developer now new goal in my life',
+            textAlign: TextAlign.center,
+          maxLines: 2,
+          style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          backgroundColor: Colors.pink,
+          color: Colors.white,
+          overflow: TextOverflow.ellipsis,
+        ),),
+         */
+
+        //  Icon
+        /* child: Icon(
+          Icons.android,
+          size: 80,
+          color: Colors.green,
+        ),
+         */
+
+
       ),
-    ),
-  ));
+    );
+  }
 
 }
 
