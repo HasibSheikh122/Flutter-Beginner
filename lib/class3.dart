faa4b02c59201ea47b58c4d912909ab9e9d56810
HasibSheikh122 <hasibsk1606@gmail.com>
2025-11-07 15:16:29 +0600

    first commit

--- lib/class3.dart
+++ lib/class3.dart
@@ -9,6 +9,32 @@ class Three extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Home Screen', style: TextStyle(
+          color: Colors.white
+        ),),
+        leading: Icon(Icons.home_filled, color: Colors.white,),
+        actions: [
+          IconButton(onPressed: (){
+           /* ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Hi I am Hasib'),
+                backgroundColor: Colors.red,
+                duration: Duration(seconds: 1),
+              ),
+            );
+            
+            */
+            
+            showDialog(context: context, builder: (context){
+              return Text('Hello world');
+            });
+          }, icon: Icon(Icons.add)
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
